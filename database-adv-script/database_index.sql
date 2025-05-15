28e63823239d5ee146aa42f7be3c72daf975c5ac
Muliro1 <mulirokhaemba@gmail.com>
2025-05-15 13:56:11 +0300

    ft-indexing

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -12,3 +12,15 @@ CREATE INDEX idx_booking_status ON Booking (status);
 CREATE INDEX idx_property_id ON Property (property_id);
 CREATE INDEX idx_property_location ON Property (location);
 
+-- Test query to measure performance
+EXPLAIN ANALYZE
+SELECT 
+    u.user_id,
+    u.first_name,
+    u.last_name,
+    COUNT(b.booking_id) as total_bookings
+FROM "User" u
+LEFT JOIN Booking b ON u.user_id = b.user_id
+WHERE b.status = 'confirmed'
+GROUP BY u.user_id, u.first_name, u.last_name
+ORDER BY total_bookings DESC;
