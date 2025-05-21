b23ad88b0114af3e6ef7726c7260683255124825
Muliro1 <mulirokhaemba@gmail.com>
2025-05-21 14:14:50 +0300

    fx

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -21,4 +21,5 @@ FROM Booking b
 JOIN "User" u ON b.user_id = u.user_id
 JOIN Property p ON b.property_id = p.property_id
 LEFT JOIN Payment pay ON b.booking_id = pay.booking_id
-ORDER BY b.start_date DESC;
+WHERE b.status = 'confirmed'
+  AND b.start_date >= '2024-06-01'
